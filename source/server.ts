1e9760c20c804a9feb92f822f4694947e9860860
khaldoun.lakiss <khaldoun.lakiss@DEVLAPTOP>
2021-02-08 00:35:33 +0200

    feat: add swagger ui

--- source/server.ts
+++ source/server.ts
@@ -4,12 +4,14 @@ import express from 'express';
 import logging from './config/logging';
 import config from './config/config';
 import distanceRoutes from './routes/distance';
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const NAMESPACE = 'Server';
-const router = express();
+const app = express();
 
 /** log requests */
-router.use((req, res, next) => {
+app.use((req, res, next) => {
     logging.info(NAMESPACE, `METHOD - [${req.method}], URL - [${req.url}], IP - [${req.socket.remoteAddress}]`);
 
     res.on('finish', () => {
@@ -19,7 +21,7 @@ router.use((req, res, next) => {
 });
 
 /** Rules for the APIs */
-router.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     if (req.method == 'OPTIONS') {
@@ -30,14 +32,17 @@ router.use((req, res, next) => {
 });
 
 /** Inject body parser */
-router.use(bodyParser.urlencoded({ extended: false }));
-router.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /** Routes */
-router.use('/distance', distanceRoutes);
+app.use('/distance', distanceRoutes);
+
+/** Swagger Configuration */
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 /** Error Handling */
-router.use((req, res, next) => {
+app.use((req, res, next) => {
     const error = new Error('not found');
     return res.status(404).json({
         message: error.message
@@ -45,5 +50,5 @@ router.use((req, res, next) => {
 });
 
 /** Create the server */
-const httpServer = http.createServer(router);
+const httpServer = http.createServer(app);
 httpServer.listen(config.server.port, () => logging.info(NAMESPACE, `Server running on ${config.server.hostname}:${config.server.port}`));
